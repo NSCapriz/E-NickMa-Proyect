7cc78459a214eeb59fe2be4fb6aed1c579086814
Nicolas Sebastian Capriz <nscapriz@do.scba.gov.ar>
2024-01-04 12:46:22 -0300

    Se agrega API KEY

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import './assets/css/App.css'
 import Navbar from './assets/Components/Global/Navbar'
 import Footer from './assets/Components/Global/Footer'
+import Buscador from './assets/Components/Content/Buscador'
 
 function App() {
 
   return (
     <>
       <Navbar/>
+      <Buscador/>
       <Footer/>
     </>
   )
--- src/assets/Components/Content/Buscador.jsx
+++ src/assets/Components/Content/Buscador.jsx
@@ -1,9 +1,22 @@
 import { useState } from "react"
 
 const Buscador = () => {
+    const urlBase = 'https://api.themoviedb.org/3/search/movie'
+    const API_KEY = 'acf5472e45e436d5026bfd4c5e792981'
+
     const [buscador, setBuscador] = useState('')
     const [peliculas, setPeliculas] = useState([])
 
+    const fechPeliculas = async () =>{
+        try{
+            const response = await fetch(`${urlBase}?query=${buscador}&api_key=${API_KEY}`)
+            const data = await response.json()
+            setPeliculas(data)
+        }
+        catch (error){
+            console.error('Ha ocurrido un error: ', error)
+        }
+    }
     const handleInputChange = (e) => {
         setBuscador(e.target.value)
     }
@@ -24,6 +37,7 @@ const Buscador = () => {
                 />
                 <button type='submit' className="btn btn-primary">Buscar</button>
             </form>
+            
         </div>
     </>
   )
