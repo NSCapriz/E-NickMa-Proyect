c2c4d0bf3fd92f43c8a6e4c0059b1fb4acfd007b
Nicolás Capriz <nicocapriz@gmail.com>
2024-01-05 14:16:57 -0300

    Se completa consumo de API

--- src/assets/Components/Content/Buscador.jsx
+++ src/assets/Components/Content/Buscador.jsx
@@ -7,13 +7,13 @@ const Buscador = () => {
     const [buscador, setBuscador] = useState('')
     const [peliculas, setPeliculas] = useState([])
 
-    const fechPeliculas = async () =>{
-        try{
+    const fechPeliculas = async () => {
+        try {
             const response = await fetch(`${urlBase}?query=${buscador}&api_key=${API_KEY}`)
             const data = await response.json()
             setPeliculas(data)
         }
-        catch (error){
+        catch (error) {
             console.error('Ha ocurrido un error: ', error)
         }
     }
@@ -25,22 +25,30 @@ const Buscador = () => {
         e.preventDefault()
         fechPeliculas()
     }
-  return (
-    <>
-        <div className="container-buscador">
-            <h2>¿Qué pelicula estas buscando?</h2>
-            <form onSubmit={handleSubmit}>
-                <input type="text" 
-                placeholder='Escribí el nombre de una pelicula...'
-                value={buscador}
-                onChange={handleInputChange}
-                />
-                <button type='submit' className="btn btn-primary">Buscar</button>
-            </form>
-            
-        </div>
-    </>
-  )
+    return (
+        <>
+            <div className="container-buscador">
+                <h2>¿Qué pelicula estas buscando?</h2>
+                <form onSubmit={handleSubmit}>
+                    <input type="text"
+                        placeholder='Escribí el nombre de una pelicula...'
+                        value={buscador}
+                        onChange={handleInputChange}
+                    />
+                    <button type='submit' className="btn btn-primary">Buscar</button>
+                </form>
+                <div className="movie-list">
+                    {peliculas.map((pelicula) => (
+                        <div key={pelicula.id} className="movie-card">
+                            <img src={`https://image.tmdb.org/t/p/w500${pelicula.poster_path}`} alt={pelicula.title} />
+                            <h2>{pelicula.title}</h2>
+                            <p>{pelicula.overview}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Buscador
